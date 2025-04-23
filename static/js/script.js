f4d06d15d7153136557bb84898edfa9dd90ff530
Ferdian Adham <ferdianadham@gmail.com>
2025-04-23 10:25:01 +0700

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -6,22 +6,39 @@ function updateTrendIndicator(initialPrice, prediksi) {
         trendIndicator.style.color = '#ff5c5c';
     } else if (parseFloat(initialPrice) < parseFloat(prediksi)) {
         trendIndicator.textContent = 'Bullish';
-        trendIndicator.style.color = '#9dd99d'
+        trendIndicator.style.color = '#9dd99d';
     } else {
         trendIndicator.textContent = 'Neutral';
-        trendIndicator.style.color = 'white'
+        trendIndicator.style.color = 'white';
     }
 }
 function perubahanHarga(initialPrice, updatedPrice){
-    const priceChange = initialPrice - updatedPrice;
-    const change = document.getElementsByClassName('change');
+    const priceChange = updatedPrice - initialPrice;
+    const presentage = (priceChange/initialPrice)*100;
+    const change = document.getElementById('perubahan');
+    console.log("perubahan harga : ", priceChange);
+    console.log("perubahan presentase : ", presentage)
+    if(parseFloat(priceChange)<0){
+        change.textContent = "USD " + priceChange.toFixed(2) + " (" + presentage.toFixed(2) + "%)";
+        change.style.color = '#ff5c5c';
+        console.log("sukses");
+    }else if(parseFloat(priceChange)>0){
+        change.textContent = "USD +" + priceChange.toFixed(2)+ " (+" + presentage.toFixed(2) + "%)";
+        change.style.color = '#9dd99d';
+        console.log("sukses"); 
+    }else{
+        change.textContent = priceChange + " (" + presentage + "%)";
+        //change.style.color = 'white';
+        console.log("sukses");
+    }
 }
 
 const currentHarga = initialPriceFromTemplate;
 const predictedHarga = predictedPriceFromTemplate;
 const updatedHarga = updatedPriceFromTemplate;
 
-updateTrendIndicator(currentHarga, predictedHarga);
+updateTrendIndicator(updatedHarga, predictedHarga);
+perubahanHarga(currentHarga, updatedHarga);
 
 /*
 setInterval( () => {
